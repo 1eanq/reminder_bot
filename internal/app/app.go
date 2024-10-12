1ee8823f4545346914eb62c965efa7863604f5f1
leanq <leanqha@gmail.com>
2024-10-12 22:09:13 +0300

    added start message and register message

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,6 +3,7 @@ package app
 import (
 	"fmt"
 	"github.com/mymmrac/telego"
+	th "github.com/mymmrac/telego/telegohandler"
 	tu "github.com/mymmrac/telego/telegoutil"
 	"log/slog"
 )
@@ -36,30 +37,38 @@ func Run(app *Application) {
 		panic(err)
 	}
 
-	botUser, _ := bot.GetMe()
-	fmt.Printf("Bot User: %+v\n", botUser)
-
 	updates, _ := bot.UpdatesViaLongPolling(nil)
+
+	bh, _ := th.NewBotHandler(bot, updates)
+
+	defer bh.Stop()
+
 	defer bot.StopLongPolling()
 
-	for update := range updates {
-		keyboard := tu.Keyboard(
-			tu.KeyboardRow(
-				tu.KeyboardButton("Button A").
-					WithRequestPoll((tu.PollTypeRegular())),
-			),
-			tu.KeyboardRow(
-				tu.KeyboardButton("Contact").WithRequestContact(),
-				tu.KeyboardButton("Location").WithRequestLocation(),
-			),
-		).WithResizeKeyboard().WithInputFieldPlaceholder("Select!!!")
+	bh.HandleMessage(func(bot *telego.Bot, message telego.Message) {
+		// Send start message
+		_, _ = bot.SendMessage(tu.Message(
+			tu.ID(message.Chat.ID),
+			fmt.Sprintf("Hello %s! Press REGISTER to start", message.From.FirstName),
+		).WithReplyMarkup(tu.InlineKeyboard(
+			tu.InlineKeyboardRow(
+				tu.InlineKeyboardButton("REGISTER").WithCallbackData("register"),
+			)),
+		))
+	}, th.CommandEqual("start"))
 
-		msg := tu.Message(
-			tu.ID(update.Message.Chat.ID),
-			"Hello world",
-		).WithReplyMarkup(keyboard).WithProtectContent()
+	// Register new handler with match on the call back query
+	// with data equal to `go` and non-nil message
+	bh.HandleCallbackQuery(func(bot *telego.Bot, query telego.CallbackQuery) {
+		// Send message
+		_, _ = bot.SendMessage(tu.Message(tu.ID(query.Message.GetChat().ID), "In order to register you need to send your phone number").WithReplyMarkup(
+			tu.Keyboard(tu.KeyboardRow(tu.KeyboardButton("Send number").WithRequestContact())).
+				WithResizeKeyboard().WithInputFieldPlaceholder("Send your phone number"),
+		))
 
-		bot.SendMessage(msg)
-	}
+		// Answer callback query
+		_ = bot.AnswerCallbackQuery(tu.CallbackQuery(query.ID).WithText("Done"))
+	}, th.AnyCallbackQueryWithMessage(), th.CallbackDataEqual("register"))
 
+	bh.Start()
 }
